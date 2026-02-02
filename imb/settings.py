0fca91c8888be42a7b833bfb400e65ca0a37acf7
DAH-dev <edah4648@gmail.com>
2026-02-02 19:33:57 +0100

    ajout des vues sur index

--- imb/settings.py
+++ imb/settings.py
@@ -148,6 +148,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # ← AJOUTE ÇA
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
